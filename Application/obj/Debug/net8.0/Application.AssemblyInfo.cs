c57dd7a83454c52aa5d8e68ec363215694f82cd2
HamidrezaRk <hamidreza.it.94478.4251@gmail.com>
2025-04-19 21:35:19 +0330

    init application

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a56fa5aa1905e0d688040925baf87f3692f25f19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d9e168c227230c0a4449bb0679249a32ea3c455")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
