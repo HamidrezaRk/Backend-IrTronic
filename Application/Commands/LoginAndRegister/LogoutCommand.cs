d7374c0c6b3196d459e2e0c654a8129d991c1b44
HamidrezaRk <hamidreza.it.94478.4251@gmail.com>
2025-06-18 13:53:00 +0330

    Add logout Command

---
+++ Application/Commands/LoginAndRegister/LogoutCommand.cs
@@ -0,0 +1,10 @@
+﻿using Application.BaseRequest.Interface;
+using Application.Results;
+
+namespace Application.Commands.LoginAndRegister;
+
+ public class LogoutRequest
+ {
+    public required string DeviceId { get; set; }
+ }
+public class LogoutCommand :BaseRequest<LogoutRequest ,SuccessfulResult>;
