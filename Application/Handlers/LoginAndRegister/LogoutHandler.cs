8531dcc28637da1a8ad706bcda07eedd5e3df9a0
HamidrezaRk <hamidreza.it.94478.4251@gmail.com>
2025-06-18 13:55:25 +0330

    "Added handler and controller to support logout functionality.

---
+++ Application/Handlers/LoginAndRegister/LogoutHandler.cs
@@ -0,0 +1,19 @@
+﻿using Application.Commands.LoginAndRegister;
+using Application.Results;
+using MediatR;
+using static Application.Failures.Failures;
+
+namespace Application.Handlers.LoginAndRegister;
+
+ public class LogoutHandler : IRequestHandler<LogoutCommand , SuccessfulResult>
+ {
+    public async Task<SuccessfulResult> Handle(LogoutCommand request, CancellationToken cancellationToken)
+    {
+        var userId = request.UserId;
+        if (userId is not null)
+        {
+            return new SuccessfulResult();
+        }
+        throw new UserNotFound();
+    }
+ }
---
+++ IRTBackend/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Application.Commands.LoginAndRegister;
+using Application.Results;
+using Domain.Enums;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IRTBackend.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class UserController(IMediator mediator) : BaseController.BaseController(mediator)
+    {
+        [HttpDelete("/Users/LogoutAdmin")]
+        [ProducesResponseType(typeof(SuccessfulResult), 200)]
+        [ProducesResponseType(typeof(AppBadRequestResult), 400)]
+        [ProducesResponseType(500)]
+        [Authorize(Roles = $"{PermissionConstants.AdminId},{PermissionConstants.ShopAdminId},{PermissionConstants.DispatcherId},{PermissionConstants.OperatorId},{PermissionConstants.ContentManagementId},{PermissionConstants.FinanceId}")]
+        public async Task<IActionResult> LogoutAdmin([FromQuery] LogoutRequest request)
+        {
+            return await HandleRequest(new LogoutCommand() { RequestBody = request });
+        }
+    }
+}
